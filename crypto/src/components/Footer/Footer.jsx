4de88815483b1a39e7be7acf289f748c610fd3ae
victor <victormuhoro7@gmail.com>
2024-08-10 02:29:34 +0300

    Done with most features

---
+++ crypto/src/components/Footer/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Footer.css'
+
+const Footer = () => {
+  return (
+    <div className='footer'>
+        <p>Copyright - All Rights Reserved By Victor Muhoro</p>
+    </div>
+  )
+}
+
+export default Footer
